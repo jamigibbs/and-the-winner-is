606fc2b4e4488487b9ee1782c24d923cb6c5dcdd
Jami Gibbs <jami0821@gmail.com>
2018-07-23 10:08:26 -0500

    Adding tests for framework votes public routes

---
+++ server/api/frameworks.spec.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../../server')
+const { session, driver } = require('../db')
+
+describe('Framework votes public routes', () => {
+
+  after('close db session', () => {
+    driver.close()
+    session.close()
+  })
+
+  describe('GET /api/frameworks/latest/votes', () => {
+    it('responds with 200 and all votes in the database', async () => {
+      await request(app)
+        .get('/api/frameworks/latest/votes')
+        .expect(200)
+        .then(response => {
+          expect(response.body).to.be.an('array')
+          expect(response.body).to.have.length(4)
+        })
+    })
+  })
+
+  describe('GET /api/frameworks/:countryCode/votes', () => {
+    it('responds with 200 and all votes for country US in the database', async () => {
+      await request(app)
+        .get('/api/frameworks/US/votes')
+        .expect(200)
+        .then(response => {
+          expect(response.body).to.be.an('array')
+        })
+    })
+  })
+
+})
